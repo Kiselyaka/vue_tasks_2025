af7e3e7aba1de1b3db20a7c747707163dcb7d60f
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 18:52:49 +0300

    Vue: Задание №22

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -2,39 +2,23 @@
 export default {
   data() {
     return {
-      visible1: false, 
-	  visible2: false,
-	  visible3: false,
-    };
+		isAuth: true,
+	}
   },
   methods: {
     toggle1: function() {
-      this.visible1= !this.visible1; 
+      this.isAuth= !this.isAuth; 
     },
-	toggle2: function(){
-		this.visible2= !this.visible2; 
-	},
-	toggle3: function(){
-		this.visible3= !this.visible3; 
-	},
+
   },
 };
-
+// нет, ну тебя совесть не мучает ?)
 </script>
 
 <template>
   <div>
-  <div>
-	<button @click="toggle1">{{ visible1 ? 'hide' : 'show' }}</button>
-	<p v-if="visible1">text</p>
-  </div>
-  <div>
-	<button @click="toggle2">{{ visible2 ? 'hide' : 'show' }}</button>
-	<p v-if="visible2">text</p>
-  </div>
-  <div>
-	<button @click="toggle3">{{ visible3 ? 'hide' : 'show' }}</button>
-	<p v-if="visible3">text</p>
-  </div>
+	<p v-if="isAuth">Вы действительно администратор</p>
+	<p v-else>Nonono, mr. fish, попробуйте зайти с ролью Администратор</p>
+	<button @click="toggle1">Выйти/Зайти</button>
   </div>
 </template>	
