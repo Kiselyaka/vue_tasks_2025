8a34fccf118353959c2ebc50dfadf24bc35964ac
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 20:21:27 +0300

    Vue: Задание №27

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -2,7 +2,7 @@
 export default {
   data() {
     return {
-		hide_swap: true,
+		items: [1, 2, 3, 4, 5],
     };
   },
   methods: {
@@ -16,11 +16,11 @@ export default {
 
 <template>
 	<div>
-	<div v-if="hide_swap">
-		<p>+++</p>
-		<p>+++</p>
-		<p>+++</p>
-	</div>
-	<button @click="click">Показать / Сккрыть</button>	
+		Задание 1:
+		<div v-for="elem in items">{{ elem }}</div>
+		Задание 2:
+		<div v-for="elem in items">{{ elem*elem }}</div>
+		Задание 3:
+		<ul><li v-for="elem in items">{{ elem}}</li></ul>
 	</div>
   </template>
