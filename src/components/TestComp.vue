9ece8bf86e0faf19720ec24218aff3b49c912463
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 19:35:43 +0300

    Vue: Задание №25

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -2,11 +2,13 @@
 export default {
   data() {
     return {
-		age: 25,
+		hide_swap: true,
     };
   },
   methods: {
-    
+        click: function(){
+			this.hide_swap = !this.hide_swap;
+		}
   },
 };
 
@@ -14,9 +16,11 @@ export default {
 
 <template>
 	<div>
-	<p v-if="age < 18">подросток</p>
-	<p v-else-if="age >= 19 && age <= 25">молодой человек</p>
-	<p v-else-if="age > 26">мужчина</p>
-	<p v-else>error</p>
+	<div v-if="hide_swap">
+		<p>+++</p>
+		<p>+++</p>
+		<p>+++</p>
+	</div>
+	<button @click="click">Показать / Сккрыть</button>	
 	</div>
   </template>
