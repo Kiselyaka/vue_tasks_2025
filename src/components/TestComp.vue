90859420116525d705591b43549fdd82f0add02d
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-08 18:23:39 +0300

    Vue: Задание №19

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -3,6 +3,8 @@ export default {
   data() {
     return {
       hidden: true, 
+	  z2: false,
+	  hidden_z3: true,
     };
   },
   methods: {
@@ -10,14 +12,29 @@ export default {
       this.hidden = !this.hidden; 
       console.log('Текущее значение hidden:', this.hidden);
     },
+	click1: function(){
+		this.z2 = true;
+	},
+	click2: function(){
+		this.z2 = false;
+	},
+	click_z3: function() {
+      this.hidden_z3 = !this.hidden_z3; 
+    },
   },
 };
-// не копируй, хорошо -_-
+
 </script>
 
 <template>
   <div>
     <p v-if="hidden">text, когда hidden = true</p>
-    <button @click="click">Инвертировать условие</button>
+    Задание 1: <button @click="click">Инвертировать условие</button>
+   <div>Задание 2: <p v-if="z2">Ого, абзац показался</p> 
+	<button @click="click1">Показать</button>
+	<button @click="click2">Скрыть</button></div>
+	<div>Задание 3: <p v-if="hidden_z3">Да ну, снова абзац что-ли. А кто-то все еще копирует мой код</p>
+	<button @click="click_z3">Скрыть/Показать</button>
+	</div>
   </div>
-</template>
+</template>	
