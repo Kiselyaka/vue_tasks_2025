58bbafdaa420a010299ff947b10a89ddca3bbe56
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-19 20:56:59 +0300

    Vue: Задание №34

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -2,7 +2,20 @@
 export default {
   data() {
     return {
-      items: [1, -2, 3, -4, 5],
+		products: [
+			{
+				id: 1,
+				name: 'product1',
+			},
+			{
+				id: 2,
+				name: 'product2',
+			},
+			{
+				id: 3,
+				name: 'product3',
+			},
+		]
     };
   },
 };
@@ -10,10 +23,8 @@ export default {
 
 <template>
   <ul>
-		<template v-for="elem in items">
-			<li v-if="elem >= 0">
-				{{ elem }}
-			</li>
-		</template>
+	<li v-for="prod in products" :key="prod.id">
+		{{ prod.name }}
+	</li>
 	</ul>
 </template>
