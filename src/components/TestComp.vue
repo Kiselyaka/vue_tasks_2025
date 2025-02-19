7a515abcc3dbc716089658c6d9f75fbbf1dcaecc
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-19 20:50:59 +0300

    Vue: Задание №33

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -2,38 +2,18 @@
 export default {
   data() {
     return {
-		hrefs: [
-			{href: '1.html', text: 'text1'},
-			{href: '2.html', text: 'text2'},
-			{href: '3.html', text: 'text3'},
-		],
-		products: [
-			{
-				name: 'product1',
-				price: 100,
-				quantity: 5
-			},
-			{
-				name: 'product2',
-				price: 200,
-				quantity: 4
-			},
-			{
-				name: 'product3',
-				price: 300,
-				quantity: 3
-			},
-		]
+      items: [1, -2, 3, -4, 5],
     };
   },
 };
-
 </script>
 
 <template>
-	<div>
-		<p v-for="num in 30">
-		{{ num }}
-		</p>
-	</div>
-  </template>
+  <ul>
+		<template v-for="elem in items">
+			<li v-if="elem >= 0">
+				{{ elem }}
+			</li>
+		</template>
+	</ul>
+</template>
