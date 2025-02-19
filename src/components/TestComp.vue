e928309d471d4ceb3278b514eb0cee215736535a
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-19 23:22:59 +0300

    Vue: Задание №44

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -1,36 +1,70 @@
 <script>
-  export default {
-	data() {
-	  return {
-		message: 'hello',
-		number: 0,
-	  };
-	},
-	computed: {
-    upperCaseMessage() {
-      return this.message.toUpperCase();
+export default {
+  data() {
+    return {
+      num: 0,
+      res: 0,
+	  num1: 0,
+      num2: 0,
+      sum: 0,
+	  text1: '',
+      text2: '',
+	  fullName: '',
+      lastName: '',
+      firstName: '',
+      middleName: '',
+    };
+  },
+  methods: {
+    calc() {
+      this.res = Math.sqrt(this.num);
     },
-	squaredNumber() {
-      return this.number * this.number;
+	calculateSum() {
+      this.sum = this.num1 + this.num2;
+    },
+	swapText() {
+      const temp = this.text1;
+      this.text1 = this.text2;
+      this.text2 = temp;
+    },
+	splitName() {
+      const names = this.fullName.split(' ');
+      this.lastName = names[0] || '';
+      this.firstName = names[1] || '';
+      this.middleName = names[2] || '';
     },
   },
-  };
-  </script>
+};
+</script>
 
 <template>
 	<div>
 		Задача 1:
-	  <input v-model="message">
-	  <p>{{ message }}</p>
+	  <p>{{ res }}</p>
+	  <input v-model.number="num" type="number" placeholder="Введите число">
+	  <button @click="calc">work</button>
 	</div>
 	<div>
 		Задача 2:
-    <input v-model="message">
-    <p>{{ upperCaseMessage }}</p>
+    <p>{{ sum }}</p>
+    <input v-model.number="num1" type="number" placeholder="Введите первое число">
+    <input v-model.number="num2" type="number" placeholder="Введите второе число">
+    <button @click="calculateSum">Сложить</button>
   </div>
   <div>
-	Задача 3:
-    <input type="number" v-model.number="number">
-    <p>{{ squaredNumber }}</p>
+		Задача 3:
+    <input v-model="text1" placeholder="Введите текст 1">
+    <input v-model="text2" placeholder="Введите текст 2">
+    <button @click="swapText">Обменять</button>
+    <p>Текст 1: {{ text1 }}</p>
+    <p>Текст 2: {{ text2 }}</p>
+  </div>
+  <div>
+		Задача 4:
+    <input v-model="fullName" placeholder="Введите ФИО">
+    <button @click="splitName">Разделить</button>
+    <p>Фамилия: {{ lastName }}</p>
+    <p>Имя: {{ firstName }}</p>
+    <p>Отчество: {{ middleName }}</p>
   </div>
   </template>
