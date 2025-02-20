1f489b28858a6b5095c54206858855c1f13ab126
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-20 18:51:31 +0300

    Vue: Задание №51

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -1,56 +1,27 @@
 <script>
 export default {
-  data() {
-	const today = new Date();
-    const currentYear = today.getFullYear();
-    const currentMonthIndex = today.getMonth(); 
-    const currentDate = today.getDate();
-
-    const years = Array.from({ length: 101 }, (_, i) => currentYear - 50 + i);  
-    return {
-		selected: '',
-		week: ['понедельник', 'вторник', 'среда', "четверг", "пятница", "суббота", "воскресенье"],
-		selectedDay: currentDate.toString(),
-      	selectedMonth: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'][currentMonthIndex],
-      	selectedYear: currentYear,
-      	days: Array.from({ length: 31 }, (_, i) => i + 1).map(String), 
-      	months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
-      	years
-    }
-  },
-  computed: {
-    result() {
-      return this.arr.length > 0 ? this.arr.join(', ') : 'Нет выбранных языков';
-    }
-  }
-};
+	data() {
+		return {
+			isDisabled: true,
+		}
+	},
+	methods: {
+		toggleInput() {
+			this.isDisabled = !this.isDisabled;
+		}
+	}
+}
 </script>
 
 <template>
 	<div>
 		Задача 1:
-	<p>
-	Выберите день недели: 
-	<select v-model="selected">
-		<option v-for="option in week">{{ option }}</option>
-	</select> 
-	<br />
-	</p>
-	<p>Выбранный день недели: {{ selected }}</p> </div>
+		<input type="text" v-bind:disabled="isDisabled" />
+		<button @click="toggleInput">{{ isDisabled ? 'Разблокировать' : 'Заблокировать' }}</button>
+	</div>
 	<div>
-		Задача 2-3:
-    <select v-model="selectedDay">
-      <option v-for="day in days" :key="day">{{ day }}</option>
-    </select>
-    
-    <select v-model="selectedMonth">
-      <option v-for="month in months" :key="month">{{ month }}</option>
-    </select>
-
-    <select v-model="selectedYear">
-      <option v-for="year in years" :key="year">{{ year }}</option>
-    </select>
-
-    <p>Выбранная дата: {{ selectedDay }} {{ selectedMonth }} {{ selectedYear }}</p>
-  </div>
+		Задача 2:
+		<input type="checkbox" v-model="isDisabled" /> Разрешить ввод
+		<input type="text" v-bind:disabled="isDisabled" />
+	</div>
 </template>
