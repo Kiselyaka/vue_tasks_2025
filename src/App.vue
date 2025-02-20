be619d82bbbfca1b8c8e4bf2613c5ea33463e88c
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-20 20:31:56 +0300

    Vue: Задание №58

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,24 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TestComp from './components/TestComp.vue';
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+	import Employee from './components/Employee.vue';
+  import TestComp from './components/TestComp.vue';
+
+	
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		components: {
+			TestComp,
+      Employee
+		}
+	}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-      <TestComp></TestComp>
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<TestComp />
+  <Employee />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
