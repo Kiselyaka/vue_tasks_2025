57fcf84c56f0ee9a8b4ab15595a9e8f7d0dedc91
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-20 08:22:05 +0300

    Vue: Задание №47

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -1,14 +1,39 @@
-  <script>
-  export default {
-	data() {
-	  return {
-		checked: true,
-	  }
+<script>
+export default {
+  data() {
+    return {
+      arr: [],
+    }
   },
-  };
-  </script>
+  computed: {
+    result() {
+      return this.arr.length > 0 ? this.arr.join(', ') : 'Нет выбранных языков';
+    }
+  }
+};
+</script>
 
 <template>
-	<input type="checkbox" v-model="checked">
-	<p v-if="checked">{{ checked ? 'yes' : 'no' }}</p>
+  <div>
+    <div>
+      Знаете ли вы JavaScript: <input type="checkbox" v-model="arr" value="JavaScript">
+    </div>
+    <div>
+      Знаете ли вы C++: <input type="checkbox" v-model="arr" value="C++">
+    </div>
+    <div>
+      Знаете ли вы Dart: <input type="checkbox" v-model="arr" value="Dart">
+    </div>
+    
+    <div>
+      <h3>Выбранные языки:</h3>
+      <ul>
+        <li v-for="(language, index) in arr" :key="index">{{ language }}</li>
+      </ul>
+    </div>
+
+    <div>
+      {{ result }}
+    </div>
+  </div>
 </template>
