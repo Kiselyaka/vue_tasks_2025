f5415fb2a54823ff4b77fc87167039786c4e479a
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-20 19:02:50 +0300

    Vue: Задание №52

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -1,27 +1,50 @@
 <script>
 export default {
-	data() {
-		return {
-			isDisabled: true,
-		}
+  data() {
+	return {
+	  inputText: '',
+	  displayedText: ''
+	};
+  },
+  methods: {
+	updateText() {
+	  this.displayedText = this.inputText;
+	  this.inputText = ''; 
 	},
-	methods: {
-		toggleInput() {
-			this.isDisabled = !this.isDisabled;
-		}
-	}
-}
+	handleClick(event) {
+      if (event.ctrlKey) {
+        this.displayedText = 'Клавиша Ctrl была зажата';
+      }
+    },
+	handleMouseDown(event) {
+      if (event.button === 0) {
+        this.displayedText = 'left';  
+      } else if (event.button === 2) {
+        this.displayedText = 'right'; 
+      } else if (event.button === 1) {
+        this.displayedText = 'middle'; 
+      }
+    }
+  }
+};
 </script>
 
 <template>
 	<div>
 		Задача 1:
-		<input type="text" v-bind:disabled="isDisabled" />
-		<button @click="toggleInput">{{ isDisabled ? 'Разблокировать' : 'Заблокировать' }}</button>
+	  <input v-model="inputText" @keyup.enter="updateText" placeholder="Введите текст"/>
+	  <p>{{ displayedText }}</p>
 	</div>
 	<div>
 		Задача 2:
-		<input type="checkbox" v-model="isDisabled" /> Разрешить ввод
-		<input type="text" v-bind:disabled="isDisabled" />
-	</div>
-</template>
+    <a href="#" @click.prevent="handleClick">Нажмите здесь</a>
+    <p>{{ displayedText }}</p>
+  </div>
+  <div>
+		Задача 3:
+    <a href="#" 
+       @mousedown="handleMouseDown">Нажмите на меня</a>
+    <p>{{ displayedText }}</p>
+  </div>
+  </template>
+  
