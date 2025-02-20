e99e0710e125a03df99d59b337ccd1968617e375
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-20 19:14:05 +0300

    Vue: Задание №54

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -7,11 +7,8 @@ export default {
 	};
   },
   methods: {
-	addItem: function() {
-	this.items.push(this.newItem);
-	},
-	addItem_in_end: function() {
-	this.items.unshift(this.newItem);
+	removeItem: function(index) {
+		this.items.splice(index, 1);
 	}
   }
 };
@@ -19,19 +16,12 @@ export default {
 
 <template>
 	<div>
-		Задача 1:
-	<input v-model="newItem">
-	<button @click="addItem">add</button>
-	<ul>
-		<li v-for="(item, index) in items" :key="index">
+		<ul>
+		<li v-for="(item, index) in items" :key="index" @click="removeItem(index)">
 			{{ item }}
 		</li>
 	</ul>
 	</div>
-	<div>
-		Задача 2:
-		В начало списка списка
-		<button @click="addItem_in_end">add</button>
-	</div>
 </template>
+  
   
