dbe3fe85219e7c27cf1b93ce510297516ccf9b43
Kiselyaka <kiselyaka12s@gmail.com>
2025-02-20 07:51:25 +0300

    Vue: Задание №46

--- src/components/TestComp.vue
+++ src/components/TestComp.vue
@@ -2,32 +2,13 @@
   export default {
 	data() {
 	  return {
-		text: '',
-		text2: '',
-		words: [],
-	  };
-	},
-	methods: {
-		// не работает, переделать
-    generateWordList() {
-		this.words = this.text2.trim().split(/s+/);
-    },
+		checked: true,
+	  }
   },
   };
   </script>
 
 <template>
-	<div>
-		Задание 1:
-	  <textarea v-model="text" placeholder="Введите текст"></textarea>
-	  <p>{{ text }}</p>
-	</div>
-	<div>
-		Задание 2:
-    <textarea v-model="text2" placeholder="Введите текст"></textarea>
-    <button @click="generateWordList">Сгенерировать список слов</button>
-    <ul>
-      <li v-for="(word, index) in words" :key="index">{{ word }}</li>
-    </ul>
-  </div>
-  </template>
+	<input type="checkbox" v-model="checked">
+	<p v-if="checked">{{ checked ? 'yes' : 'no' }}</p>
+</template>
